c3629c2edaf4aee29d08ba8809703a61be9a3c9f
Matheus Ferreira <matheusfgsantos31@gmail.com>
2024-12-09 16:49:05 -0300

    Add telas de nota fiscal e produtos

--- src/main/java/aplication/Tela.java
+++ src/main/java/aplication/Tela.java
@@ -1,7 +1,11 @@
 package aplication;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Tela extends Application {
@@ -12,6 +16,26 @@ public class Tela extends Application {
 
         BorderPane root = new BorderPane();
         root.setId("body");
+        root.setStyle("-fx-background-image:" +
+                "url('photos/images/backgroundpool.png')");
+
+        VBox vBox = new VBox();
+
+        Label titulo = new Label("GERENCIADOR");
+        titulo.setId("titulo");
+        vBox.getChildren().add(titulo);
+
+        Button produtos = new Button("PRODUTOS");
+        produtos.setId("button-produtos");
+        vBox.getChildren().add(produtos);
+
+        Button vendas = new Button("VENDAS");
+        vendas.setId("button-vendas");
+        vBox.getChildren().add(vendas);
+
+        root.setCenter(vBox);
+        vBox.setSpacing(10);
+        vBox.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(root, 800, 500);
         scene.getStylesheets().add(getClass().getResource
---
+++ src/main/java/aplication/nota_fiscal/NotasFiscais.java
@@ -0,0 +1,16 @@
+package aplication.nota_fiscal;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class NotasFiscais extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
