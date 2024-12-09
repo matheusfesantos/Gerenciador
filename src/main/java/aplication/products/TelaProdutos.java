acd8ae4a80b595809d6358bc0118eebac841c0eb
Matheus Ferreira <matheusfgsantos31@gmail.com>
2024-12-09 17:25:58 -0300

    Add estilização da Tela de Produtos

--- src/main/java/aplication/products/TelaProdutos.java
+++ src/main/java/aplication/products/TelaProdutos.java
@@ -1,16 +1,41 @@
 package aplication.products;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TelaProdutos extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) {
 
+        BorderPane root = new BorderPane();
+        root.setId("body");
+        root.setStyle("-fx-background-image:" +
+                "url('photos/images/backgroundpool.png')");
+
+        VBox vbox = new VBox();
+        vbox.setId("vbox");
+        vbox.setSpacing(10);
+        vbox.setPrefWidth(700);
+        vbox.setPrefHeight(400);
+        vbox.setMaxWidth(700);
+        vbox.setMaxHeight(400);
+
+        HBox hbox = new HBox();
+        hbox.setSpacing(10);
+
+        root.setCenter(vbox);
+
+        Scene scene = new Scene(root, 900,500);
+        scene.getStylesheets().add(getClass().getResource("/Style/TelaProdutos.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("MY STORE");
+        primaryStage.show();
     }
 }
