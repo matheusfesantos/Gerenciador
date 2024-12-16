93c5381d2f702c440b55a1f495bd7c321d459d54
Matheus Ferreira <matheusfgsantos31@gmail.com>
2024-12-16 19:58:04 -0300

    Melhoria na transição de dados entre a função de add o produto e o backend

--- src/main/java/entites/produtos/adiconar/Adicionar.java
+++ src/main/java/entites/produtos/adiconar/Adicionar.java
@@ -9,37 +9,61 @@ import java.util.Scanner;
 public class Adicionar {
 
     static String produto;
-    static TextField preco;
-    static TextField quantidade;
-    static String imagem;
-
+    static double preco;
+    static int quantidade;
     public Adicionar(TextField produto, TextField preco, TextField quantidade){
         this.produto = String.valueOf(produto);
-        this.preco = preco;
-        this.quantidade = quantidade;
-        this.imagem = imagem;
+        this.preco = Double.parseDouble(preco.getText());
+        this.quantidade = Integer.parseInt(quantidade.getText());
     }
 
-    @Override
-    public String toString() {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Scanner sc = null;
 
         String banco = "C:\\Users\\matheus.fgs\\Desktop\\" +
-                "Gerenciador_Java\\Banco_de_Dados\\produto.txt";
+                "Gerenciador_Java\\Banco_De_Dados\\produtos.txt";
 
         try(BufferedWriter br = new BufferedWriter(new FileWriter(banco, true))) {
 
-            for (String escrever : produto.split("\n")){
-                br.newLine();
-                br.newLine();
-                br.write(escrever+";");
-                br.write(preco+";");
-                br.write(quantidade+";");
-                br.write(imagem);
+            if(produto != null && preco > 0 && quantidade > 0){
+                for (String escrever : produto.split("\n")){
+                    br.newLine();
+                    br.newLine();
+                    br.write(escrever+";");
+                    br.write(preco+";");
+                    br.write(quantidade+";");
+                }
+            }else{
+                System.out.print("Valores nulos !!");
             }
+
         }catch (IOException e){
             System.out.println("Erro: " + e.getMessage());
         }
-        return"";
+    }
+    @Override
+    public String toString() {
+        String banco = "C:\\Users\\matheus.fgs\\Desktop\\" +
+                "Gerenciador_Java\\Banco_De_Dados\\produtos.txt";
+
+        try(BufferedWriter br = new BufferedWriter(new FileWriter(banco, true))) {
+
+            if(produto != null && preco > 0 && quantidade > 0){
+                for (String escrever : produto.split("\n")){
+                    br.newLine();
+                    br.newLine();
+                    br.write(escrever+";");
+                    br.write(preco+";");
+                    br.write(quantidade+";");
+                }
+            }else{
+                System.out.print("Valores nulos !!");
+            }
+
+        }catch (IOException e){
+            System.out.println("Erro: " + e.getMessage());
+        }
+        return "Produtos adicionados com sucesso.";
     }
 }
